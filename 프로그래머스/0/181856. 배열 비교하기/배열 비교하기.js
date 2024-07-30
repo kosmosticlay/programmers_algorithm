3c9ef51edc49716faf9b2a06a53bfc6520401e73
Hyemin Clay Lee <kosmosticlay@gmail.com>
2024-07-30 21:41:29 +0900

    [level 0] Title: 배열 비교하기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.js
@@ -0,0 +1,10 @@
+function solution(arr1, arr2) {
+    if (arr1.length !== arr2.length) {
+        return arr1.length > arr2.length ? 1 : -1;
+    } else {
+        const arr1Sum = arr1.reduce((acc, cur) => acc + cur, 0);
+        const arr2Sum = arr2.reduce((acc, cur) => acc + cur, 0);
+        if (arr1Sum === arr2Sum) return 0;
+        return arr1Sum > arr2Sum ? 1 : -1;
+    }
+}
