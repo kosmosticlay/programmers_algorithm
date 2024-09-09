b31b78d9aa667c4090cf99cdd80b5ec10b476161
Hyemin Clay Lee <kosmosticlay@gmail.com>
2024-09-09 14:16:46 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,6 @@
+function solution(num_list) {
+    const odd = num_list.reduce((acc, cur, i) => i % 2 !== 0 ? acc + cur : acc, 0)
+    const even = num_list.reduce((acc, cur, i) => i % 2 === 0 ? acc + cur : acc, 0)
+    
+    return Math.max(odd, even);
+}
