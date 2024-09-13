a0810d2593afbe6ee239bcdca6b099d384a4d743
Hyemin Clay Lee <kosmosticlay@gmail.com>
2024-09-13 15:13:12 +0900

    [level 0] Title: 1로 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.js
@@ -0,0 +1,10 @@
+function solution(num_list) {
+    let count = 0;
+    num_list.forEach(el => {
+        while (el !== 1) {
+            el = el % 2 ? (el - 1) / 2 : el / 2;
+            count++;
+        }
+    });
+    return count;
+}
